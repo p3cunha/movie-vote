8abad634b9b071d19b96883a3dd1f522ac384966
p3cunha <pcls_@hotmail.com>
2020-10-08 13:41:53 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'linna-dashboard';
+
+  indexInicial: number = -1
+
+  vote(index){
+    this.indexInicial === -1? this.indexInicial = index : this.indexInicial = -1  
+  }
+
+  voteUnvote(index){
+    if (this.indexInicial === index){
+      return 'Unvote'
+    } else {
+      return 'Vote'
+    }
+  }
+
+  escolhido(index){
+    return this.indexInicial === index
+  }
+
+  outroEscolhido(){
+    return this.indexInicial !== -1
+  }
 }
